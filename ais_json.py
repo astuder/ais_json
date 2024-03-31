0da157be26129a9a7f53c4c7c621a72f44def608
Adrian Studer <github@adrianstuder.com>
2024-03-30 18:00:41 -0700

    Fix prints for compatibility with Python 3

--- ais_json.py
+++ ais_json.py
@@ -84,11 +84,11 @@ while True:
       r = requests.post(URL, files={'jsonais': (None, post)})
       #dump non common packets for debugging
       if parsed['id'] not in (1,2,3,4):
-        print colored('-- Uncommon packet recieved\n', 'red')
-        print colored('id:', 'green'), parsed['id']
-        print colored('NMEA:', 'green'), parsed['nmea']
-        print colored('Parsed:', 'green'), parsed
-        print colored('Post:', 'green'), post
-        print colored('Result:', 'green'), json.loads(r.text)['description']
+        print(colored('-- Uncommon packet recieved\n', 'red'))
+        print(colored('id:', 'green'), parsed['id'])
+        print(colored('NMEA:', 'green'), parsed['nmea'])
+        print(colored('Parsed:', 'green'), parsed)
+        print(colored('Post:', 'green'), post)
+        print(colored('Result:', 'green'), json.loads(r.text)['description'])
     except requests.exceptions.RequestException as e:
-      print e
+      print(e)
